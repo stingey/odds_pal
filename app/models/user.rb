0d2d5e471eb3e677bcd551f6016b93f39f781fb9
Spenser <sftingey@gmail.com>
2023-06-23 14:16:50 -0600

    Add space

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :user_favorites, dependent: :destroy
   has_many :sports, through: :user_favorites
 end
