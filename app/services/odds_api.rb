0192afbd05d9e43c640a0d1cf846c5fd78693e13
Spenser <sftingey@gmail.com>
2023-06-23 13:50:22 -0600

    uninclude httparty

--- app/services/odds_api.rb
+++ app/services/odds_api.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 class OddsApi
-  include HTTParty
-  base_uri Rails.application.credentials.the_odds_api.base_uri
+  # # include HTTParty
+  # base_uri Rails.application.credentials.the_odds_api.base_uri
 
-  def initialize
-    @api_key = Rails.application.credentials.the_odds_api.api_key
-  end
+  # def initialize
+  #   @api_key = Rails.application.credentials.the_odds_api.api_key
+  # end
 
-  def all_active_sports
-    Rails.cache.fetch('active_sports', expires_in: 23.hours) do
-      self.class.get("/v4/sports?apiKey=#{@api_key}&all=false").to_json
-    end
-  end
+  # def all_active_sports
+  #   Rails.cache.fetch('active_sports', expires_in: 23.hours) do
+  #     self.class.get("/v4/sports?apiKey=#{@api_key}&all=false").to_json
+  #   end
+  # end
 
-  def all_sports
-    self.class.get("/v4/sports?apiKey=#{@api_key}&all=true")
-  end
+  # def all_sports
+  #   self.class.get("/v4/sports?apiKey=#{@api_key}&all=true")
+  # end
 
-  def odds_for_sport(sport)
-    Rails.cache.fetch("/sports/#{sport.key}/odds", expires_in: 23.hours) do
-      self.class.get("/v4/sports/#{sport.key}/odds/?apiKey=#{@api_key}&regions=us&oddsFormat=american&bookmakers=draftkings").to_json
-    end
-  end
+  # def odds_for_sport(sport)
+  #   Rails.cache.fetch("/sports/#{sport.key}/odds", expires_in: 23.hours) do
+  #     self.class.get("/v4/sports/#{sport.key}/odds/?apiKey=#{@api_key}&regions=us&oddsFormat=american&bookmakers=draftkings").to_json
+  #   end
+  # end
 end
