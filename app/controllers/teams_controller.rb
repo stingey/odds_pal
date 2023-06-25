8e69f387ddc2357e0437250a3826b89612c2019f
Spenser <sftingey@gmail.com>
2023-06-25 17:02:52 -0600

    Add teams controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class TeamsController < ApplicationController
+  def index
+    # sports = Sport.with_rank
+    # @favorited_sports = Sport.joins(:user_favorites).where(user_favorites: { user_id: User.first.id }).distinct
+    # @sports = sports - @favorited_sports
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :sports, only: %i[index]
+  resources :teams, only: %i[index]
   resources :favorite_sports, only: %i[create destroy]
   # Defines the root path route ("/")
   root "sports#index"
